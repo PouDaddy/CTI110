870bac50c8ff6101c80abd6fed316d085670020f
PouDaddy <145816882+PouDaddy@users.noreply.github.com>
2023-10-11 22:38:12 -0400

    Add files via upload

--- P1HW2_SmithLandon.py
+++ P1HW2_SmithLandon.py
@@ -3,23 +3,25 @@
 #CTI -110 P1HW2- Travel Expense
 #Landon Smith
 print("This program calculates and displays travel expenses")
-budget = int(input("Please enter your budget:"))
-Destination = input("Please enter your travel destination:")
-gas = int(input("Please enter how much you will spend on gas:"))
-Accommodations = int(input("Please enter how much you will spend on accommodations:"))
-Food = int(input("Please enter how much you will spend on food:"))
+budget = float(input("Enter Budget:"))
+Destination = input("Enter your travel destination:")
+gas = float(input("How much do you think you will spend on gas?"))
+Accommodations = float(input("Approximately, how much will you need for accomodation/hotel?"))
+Food = float(input("Last, how much do you need for food?"))
 total = gas + Accommodations + Food 
 remaining = budget - total
-
+formatted_budget = f"${budget:.1f}"
+formatted_fuel = f"${gas:.1f}"
+formatted_accomodation = f"${Accommodations:.1f}"
+formatted_food = f"${Food:.1f}"
+formatted_balance= f"${remaining:.1f}"
 #print ("THE TOTAL IS:",total)
-print('''        Travel Expenses        ''')
-print("Location:", Destination)
-print("Intial budget:", budget)
+print('''----------Travel Expenses----------''')
+print("Location:",Destination)
+print("Intial budget:",formatted_budget)
+print("Fuel:",formatted_fuel)
+print("Accomodations:",formatted_accomodation)
+print("Food:",formatted_food)
+print("------------------------------------------")
 print()
-print("Fuel:", gas)
-print("Accommodations:", Accommodations)
-print("Food:", Food)
-print()
-print("Remaining Balance:",remaining)
-
-
+print("Remaining Balance:",formatted_balance)
